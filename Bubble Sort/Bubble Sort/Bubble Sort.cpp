4d1d645436013c56b88dc04a8759324817289341
Raiden Morris <104537165+RaidenTWM@users.noreply.github.com>
2022-05-04 14:35:31 +1000

    Function fixes
    - Changed the names of functions to be good practices
    - Added code to the swap function
    - Changed the Bubble Sort function to make it ascending
    - Optimized the print array function
    - Made a Bigger function that just checks if one value is larger than the other

--- Bubble Sort/Bubble Sort/Bubble Sort.cpp
+++ Bubble Sort/Bubble Sort/Bubble Sort.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 
-void bubbleSort(int array[], int n);
-void printArray(int array[], int n);
-void swap(int& a, int& b);
+void BubbleSort(int array[], int n);
+void PrintArray(int array[], int n);
+void Swap(int& a, int& b);
+bool Bigger(int& a, int& b);
 
 int main()
 {
     const int array_size = 50;
     int array_to_be_sorted[array_size] = {14,65,63,1,54,89,84,9,98,57,71,18,21,84,69,28,11,83,13,42,64,58,78,82,13,9,96,14,39,89,40,32,51,85,48,40,23,15,94,93,35,81,1,9,43,39,15,17,97,52};
     std::cout << "Unsorted" << std::endl;
-    printArray(array_to_be_sorted, array_size);
-    bubbleSort(array_to_be_sorted, array_size);
+    PrintArray(array_to_be_sorted, array_size);
+    BubbleSort(array_to_be_sorted, array_size);
     std::cout << "Bubble Sort" << std::endl;
-    printArray(array_to_be_sorted, array_size);
+    PrintArray(array_to_be_sorted, array_size);
     system("pause");
     return 0;
 }
 
-void bubbleSort(int array[], int n)
+void BubbleSort(int array[], int n)
 {
     //Bail for single element or invalid indices
     if (n < 2) { return; }
@@ -28,29 +29,33 @@ void bubbleSort(int array[], int n)
         sorted = true;
         for (int i = 0; i < n - 1; ++i)
         {
-            if (array[i] < array[i + 1])
+            if (Bigger(array[i], array[i + 1]))
             {
-                swap(array[i], array[i + 1]);
+                Swap(array[i], array[i + 1]);
                 sorted = false;
             }
         }
     }
 }
 
-void printArray(int array[], int n)
+void PrintArray(int array[], int n)
 {
-    int count = 0;
-    for (int i = 0; i < n/5; i++)
+    for (int i = 0; i < n; ++i)
     {
-        for (int j = 0; j < n / 10; j++)
-        {
-            std::cout << array[count] << " ";
-            count++;
-        }
-        std::cout << std::endl;
+        std::cout << array[i] << ",\t";
+        if (i % 10 == 9) { std::cout << std::endl; }
     }
+    std::cout << std::endl;
+    std::cout << std::endl;
 }
-void swap(int& a, int& b)
+void Swap(int& a, int& b)
 {
-
+    int temp = a;
+    a = b;
+    b = temp;
+}
+bool Bigger(int& a, int& b)
+{
+    if (a > b) { return true; }
+    return false;
 }
